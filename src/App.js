0cf24e693f2973f62cbb4f4a8919834dfe7a5933
rolanusonov <rolanusonov031209@gmail.com>
2023-07-03 12:37:02 +0900

    initial aykhal akva

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Hero from "./companents/Hero";
+import Advantages from "./companents/Advantages";
+import Main from "./companents/Main";
+import Water from "./companents/Water";
+
+
+function App() {
+    return (
+        <div>
+            <Hero/>
+            <Advantages/>
+            <Main/>
+            <Water/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/companents/Water.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Water = () => {
+    return (
+ <div id="Water">
+          <div className="water">
+              <video
+                 className="WaterVideo"
+                 playsInline
+                 loop
+                 muted
+                 autoPlay
+                 alt="All the devices"
+                 src="https://media.istockphoto.com/id/1387997171/ru/%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE/3d-%D1%81%D0%B8%D0%BD%D1%8F%D1%8F-%D0%B2%D0%BE%D0%BB%D0%BD%D0%B0-%D0%B2%D1%8B%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%B0-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC-%D1%84%D0%BE%D0%BD%D0%B5-%D0%B0%D0%B1%D1%81%D1%82%D1%80%D0%B0%D0%B3%D0%B8%D1%80%D1%83%D0%B9%D1%82%D0%B5-%D1%87%D0%B8%D1%81%D1%82%D1%8B%D0%B9-%D1%81%D0%B8%D0%BD%D0%B8%D0%B9-%D0%BF%D0%BE%D1%82%D0%BE%D0%BA-%D0%B8%D0%BB%D0%B8-%D1%81%D1%82%D1%80%D1%83%D0%B9%D0%BD%D1%83%D1%8E-%D0%B6%D0%B8%D0%B4%D0%BA%D1%83%D1%8E.mp4?s=mp4-640x640-is&k=20&c=Y-y21mkIZYH-trcYqD3yoWWaNSbBmvzXYXJjDNQl11w="
+             />
+         </div>
+
+  </div>
+    );
+};
+
+export default Water;
